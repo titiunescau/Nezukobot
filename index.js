3882a995d12e972b90f4138e74f35edfab593db3
titiunescau <81128483+titiunescau@users.noreply.github.com>
2021-05-21 08:33:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -865,7 +865,7 @@ if (text.includes("placa"))
 	            case 'menu':
 	            case 'help':
 	            	uptime = process.uptime ()
-                    putagg = await getBuffer(`https://ibb.co/C8sCdYk.jpg`)
+                    putagg = await getBuffer(`https://i.ibb.co/W5p89Mb/nezukobot.jpg`)
                     client.sendMessage(from, putagg, image, {quoted: mek, caption: help(prefix, sender, pushname, time)})
                     break
                 case 'figu':
